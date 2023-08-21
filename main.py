a5f47fea88cbe347ee1a46a32de8f5c03a117c3a
Nicolas Gomez Echeverri <nigomeze@unal.edu.co>
2023-08-20 22:06:23 -0500

    entrega taller intento 1
    le rezo a Monty Python para que lo acepte a la primera

---
+++ main.py
@@ -0,0 +1,43 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        self.color = color
+
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+    
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self):
+        if (self.motor.registro != self.registro):
+            return "Las piezas no son originales"
+        for asiento in self.asientos:
+            if (asiento.registro != self.registro):
+                return "Las piezas no son originales"
+        return "Auto original"
+    
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
+    
